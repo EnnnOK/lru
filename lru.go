5b06180584e2403e66982d36fd864c881d435ca3
nuczzz <nuc_zzz@163.com>
2019-01-10 20:52:41 +0800

    function of lru

--- lru.go
+++ lru.go
@@ -1,18 +1,23 @@
 package lru
 
-import "time"
+import (
+	"time"
+)
 
 // Node lru data node
 type Node struct {
 	// Key key of node
 	Key interface{}
+	// length length of value
+	length int //todo
 	// Value value of node
 	Value interface{}
-	// Extra extra field of node
+	// Extra extra field of node. todo: this field
 	Extra interface{}
 	// AccessTime timestamp of access time
 	AccessTime int64
-	// AccessCount access count
+	// AccessCount access count.
+	// todo: current version this field is not used to sort, it just count.
 	AccessCount int64
 	// expire timestamp of expire if this field greater than 0,
 	// and if node is expired, delete from lru linked list.
@@ -24,59 +29,72 @@ type Node struct {
 }
 
 type LRU struct {
+	// maxSize all value max size of lru
+	maxSize int64
+	// curSize current all value size of lru
+	curSize int64
+
 	// ttl time to live(second)
 	ttl int64
 
-	// ExpireCallBack if this field is not nil, when a node is expired,
-	// delete it from lru linked list and callback this function.For example,
-	// in http cache, we often use map+lru-list to cache some static files,
-	// if we just delete data from lru list but not delete metadata in map,
-	// we may get wrong result.So we can write a callback function like:
+	// deleteNodeCallBack if this field is not nil, when a node is deleted,
+	// from lru linked list and callback this function.For example, in http
+	// cache, we often use map+lru-list to cache some static files, if we
+	// just delete data from lru list but not delete metadata in map, we may
+	// get wrong result. So we can write a callback function like:
 	// var globalMap = make(map[interface{}]*Node)
 	// func DeleteMetadata(key interface{}) {
 	// 		delete(globalMap, key)
 	// }
 	// lru := NewLRUWithCallback(300, DeleteMetadata)
-	expireCallBack func(key interface{})
+	deleteNodeCallBack func(key interface{})
 
+	// lru double linked list header and tail pointer.
 	header *Node
 	tail   *Node
 }
 
 // NewLRU return a new LRU instance, set time-to-live of lru node if ttl is greater than 0.
-func NewLRU(ttl int64) *LRU {
-	lru := &LRU{}
+func NewLRU(maxSize, ttl int64) *LRU {
+	lru := &LRU{maxSize: maxSize}
 	if ttl > 0 {
 		lru.ttl = ttl
 	}
 	return lru
 }
 
-// NewLRUWithCallback return a new LRU instance with expire callback.
+// NewLRUWithCallback return a new LRU instance with delete-node-callback.
 func NewLRUWithCallback(ttl int64, callback func(interface{})) *LRU {
-	lru := &LRU{expireCallBack: callback}
+	lru := &LRU{deleteNodeCallBack: callback}
 	if ttl > 0 {
 		lru.ttl = ttl
 	}
 	return lru
 }
 
-func (lru *LRU) NewNode(key, value interface{}, extra ...interface{}) *Node {
-	node := &Node{
-		Key:   key,
-		Value: value,
-	}
-	if lru.ttl > 0 {
-		node.expire = time.Now().Unix() + lru.ttl
-	}
+// getInterfaceLength get length of interface
+func getInterfaceLength(i interface{}) int {
+	// todo
+	return 0
+}
 
+func newNode(key, value interface{}, extra ...interface{}) *Node {
+	node := &Node{
+		Key:    key,
+		length: getInterfaceLength(value),
+		Value:  value,
+	}
 	if len(extra) > 0 {
 		node.Extra = extra[0]
 	}
 	return node
 }
 
-func (lru *LRU) Add(node *Node) {
+func (lru *LRU) add(node *Node) {
+	if lru.ttl > 0 {
+		node.expire = time.Now().Unix() + lru.ttl
+	}
+
 	if lru.header == nil {
 		lru.header = node
 		lru.tail = node
@@ -87,7 +105,12 @@ func (lru *LRU) Add(node *Node) {
 	}
 }
 
-func (lru *LRU) removeToHead(node *Node) {
+func (lru *LRU) NewNode(key, value interface{}, extra ...interface{}) {
+	lru.add(newNode(key, value, extra...))
+}
+
+// RemoveToHead move node to lru double linked list head
+func (lru *LRU) moveToHead(node *Node) {
 	if node != lru.header {
 		if node == lru.tail {
 			lru.tail = lru.tail.previous
@@ -103,6 +126,55 @@ func (lru *LRU) removeToHead(node *Node) {
 	}
 }
 
+func (lru *LRU) Get(node *Node) *Node {
+	now := time.Now().Unix()
+	if node.expire < now {
+		lru.Delete(node)
+		return nil
+	}
+	lru.moveToHead(node)
+	node.AccessTime = now
+	node.AccessCount++
+
+	//todo: define get node info with interface
+	return node
+}
+
+// todo
+func (lru *LRU) eliminate(length int64) {
+	for lru.tail != nil && length > 0 {
+		node := lru.tail
+		length -= int64(node.length)
+		lru.Delete(node)
+	}
+}
+
+// Delete delete node from lru double linked list,
+// node MUST not nil and is REAL node in lru list.
 func (lru *LRU) Delete(node *Node) {
-	// todo
+	if lru.header == lru.tail {
+		if lru.tail == node {
+			lru.header = node.next
+			lru.header.previous = nil
+			node.next = nil
+		} else if lru.header == node {
+			lru.tail = node.previous
+			lru.tail.next = nil
+			node.previous = nil
+		} else {
+			node.previous.next = node.next
+			node.next.previous = node.previous
+			node.previous = nil
+			node.next = nil
+		}
+	} else {
+		// just one node in lru double linked list
+		lru.header = nil
+		lru.tail = nil
+	}
+
+	// delete node callback
+	if lru.deleteNodeCallBack != nil {
+		lru.deleteNodeCallBack(node.Key)
+	}
 }
--- lru_test.go
+++ lru_test.go
@@ -1,3 +1,44 @@
 package lru
 
-// todo
+import (
+	"testing"
+	"time"
+)
+
+func TestNewLRU(t *testing.T) {
+	lru := NewLRU(100, 500)
+	t.Logf("%#v", lru)
+}
+
+func TestNewLRUWithCallback(t *testing.T) {
+	callback := func(interface{}) {}
+	lru := NewLRUWithCallback(0, callback)
+	t.Logf("%#v", lru)
+}
+
+func (lru *LRU) printLRUList(t *testing.T) {
+	temp := lru.header
+	for temp != nil {
+		t.Logf("%#v", temp)
+		temp = temp.next
+	}
+}
+
+func TestNewNode(t *testing.T) {
+	lru := NewLRU(100, 100)
+	lru.NewNode(123, 234)
+	lru.NewNode(1231, 2342)
+	lru.printLRUList(t)
+}
+
+func TestGet(t *testing.T) {
+	lru := NewLRU(100, 1)
+	node1 := newNode(123, 234)
+	node2 := newNode(1231, 2342)
+	lru.add(node1)
+	lru.add(node2)
+	lru.printLRUList(t)
+	time.Sleep(2 * time.Second)
+	t.Log(lru.Get(node1))
+	t.Log(lru.Get(node2))
+}
